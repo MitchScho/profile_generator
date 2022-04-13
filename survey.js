e0739b83050b33ab726430d81773e7704d82b70f
MitchScho <scho.mitch@gmail.com>
2022-04-13 04:09:46 +0000

    survey update

--- survey.js
+++ survey.js
@@ -6,13 +6,13 @@ const rl = readline.createInterface({
 });
 
 rl.question("What is your name? Nicknames are also acceptable :) ",(answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+  console.log(`That's a great name thanks for sharing: ${answer}`);
     rl.question("What's an activity you like to do? ", (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`);
+      console.log(`Thank you sounds courageous: ${answer}`);
         rl.question("What do you like to listen to while you do that? ",(answer) => {
-          console.log(`Thank you for your valuable feedback: ${answer}`);
+          console.log(`Nice I can feel the vibrations: ${answer}`);
             rl.question("Which meal during the day is you favourite? ", (answer) => {
-              console.log(`Thank you for your valuable feedback: ${answer}`);
+              console.log(`Great thats an amazing meal!: ${answer}`);
                 rl.question("Which meal during the day is you favourite? ",(answer) => {
                   console.log(`Thank you for your valuable feedback: ${answer}`);
                     rl.question("What's your favourite thing to eat for that meal? ",(answer) => {
